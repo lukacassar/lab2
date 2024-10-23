1650281b97e2d397512bb33c3a0dd3afafacd99d
lukacassar <luka.cassar.24@um.edu.mt>
2024-10-23 23:47:06 +0200

    fixed

--- Question5.c
+++ Question5.c
@@ -1,3 +1,24 @@
-//
-// Created by luka on 10/23/24.
-//
+#include <stdio.h>
+
+#define MAXNUM 10
+
+int main() {
+    printf("Enter 10 numbers of days\n");
+    double days[MAXNUM];
+    int weeks[MAXNUM];
+    int remDays[MAXNUM];  // remaining days after calculating weeks
+
+    // Input loop
+    for (int i = 0; i < MAXNUM; i++) {
+        scanf("%lf", &days[i]);
+    }
+    for (int i = 0; i < MAXNUM; i++) {
+        weeks[i] = (int)(days[i] / 7);
+        remDays[i] = (int)(days[i]) % 7;
+    }
+    for (int i = 0; i < MAXNUM; i++) {
+        printf("%lf days is %d weeks and %d days.\n", days[i], weeks[i], remDays[i]);
+    }
+
+    return 0;
+}
